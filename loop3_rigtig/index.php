50dfb5511ff0e98b2f2141d8d98e4daac7c8b3b8
Simon Schou Sørensen <92392637+Siimonschou@users.noreply.github.com>
2021-11-08 14:22:34 +0100

    Add files via upload

---
+++ loop3_rigtig/index.php
@@ -0,0 +1,16 @@
+<?php require_once('includes/header.php');?>
+
+<h1>Hej</h1>
+<h1>Hej</h1>
+<h1>Hej</h1>
+<h1>Hej</h1>
+
+<h1>Hej</h1>
+<h1>Hej</h1>
+<h1>Hej</h1>
+<h1>Hej</h1>
+<h1>Hej</h1>
+
+
+
+<?php require_once('includes/footer.php');?>
