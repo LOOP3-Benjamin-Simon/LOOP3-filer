4f9dfdbcf4a55521dca4181a33c3a57d86eba691
Longerich1 <93331491+Longerich1@users.noreply.github.com>
2021-11-12 13:24:28 +0100

    Add files via upload

---
+++ loop3_rigtig/includes/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer-->
+<footer class="bg-dark py-4 mt-auto" id="footer">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; StrainMonitoring BDE 3. semester 2021</div></div>
+                    <div class="col-auto">
+                        <a class="link-light small" href="#!">GDPR</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Terms of Service</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="kontakt_os.php">Kontakt</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="admin_index.php">Admin</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
